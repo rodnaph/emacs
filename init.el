273587d0b0dbf68c733e05655e2fd8b9562ba754
rod <rod@pu-gh.com>
2012-11-05 21:28:50 +0000

    changed path to match zsh (thanks @owainlewis) and changed color theme back to solarized

--- init.el
+++ init.el
@@ -1,5 +1,13 @@
 
 (add-to-list 'load-path "~/.emacs.d/")
+(add-to-list 'load-path "~/.emacs.d/themes/solarized")
+
+(let ((path (shell-command-to-string ". ~/.zshrc; echo -n $PATH")))
+  (setenv "PATH" path)
+  (setq exec-path 
+        (append
+         (split-string-and-unquote path ":")
+         exec-path)))
 
 (require 'package)
 (add-to-list 'package-archives 
@@ -11,6 +19,7 @@
 (require 'php-mode)
 (require 'color-theme)
 (require 'color-theme-molokai)
+(require 'color-theme-solarized)
 
 (global-set-key (kbd "M-3") 
   '(lambda () (interactive) (insert "#")))
@@ -22,4 +31,4 @@
           (delq (current-buffer) 
                 (remove-if-not 'buffer-file-name (buffer-list)))))
 
-(color-theme-molokai)
+(color-theme-solarized-dark)
